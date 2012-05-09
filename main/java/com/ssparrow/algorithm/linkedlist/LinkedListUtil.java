855a58e80a449791e9c13d9a2722ffcf19f1c235
Fei Gao <eaglelion8038@hotmail.com>
2012-05-08 23:35:27 -0700

    add method to calculate sum of two number that are stored as linked list

--- main/java/com/ssparrow/algorithm/linkedlist/LinkedListUtil.java
+++ main/java/com/ssparrow/algorithm/linkedlist/LinkedListUtil.java
@@ -23,4 +23,43 @@ public class LinkedListUtil {
 		}
 		return followingNode;
 	}
+	
+	/**
+	 * @param head1
+	 * @param head2
+	 * @return
+	 */
+	public static LinkedListNode addNumbers(LinkedListNode head1, LinkedListNode head2){
+		int carry=0;
+		LinkedListNode node1=head1;
+		LinkedListNode node2=head2;
+		
+		int sum=Integer.parseInt(node1.getData().toString())+Integer.parseInt(node2.getData().toString());
+		LinkedListNode head=new LinkedListNode(String.valueOf(sum%10));
+		carry=sum/10;
+		
+		while(node1.getNext()!=null || node2.getNext()!=null){
+			sum=carry;
+			
+			if(node1.getNext()!=null){
+				sum+=Integer.parseInt(node1.getNext().getData().toString());
+			}
+			
+			if(node2.getNext()!=null){
+				sum+=Integer.parseInt(node2.getNext().getData().toString());
+			}
+			
+			head.addToTail(String.valueOf(sum%10));
+			carry=sum/10;
+			
+			node1=node1.getNext();
+			node2=node2.getNext();
+		}
+		
+		if(carry>0){
+			head.addToTail(String.valueOf(carry));
+		}
+		
+		return head;
+	}
 }
--- test/java/com/ssparrow/algorithm/linkedlist/LinkedListUtilTest.java
+++ test/java/com/ssparrow/algorithm/linkedlist/LinkedListUtilTest.java
@@ -19,5 +19,30 @@ public class LinkedListUtilTest {
 		kthNode = LinkedListUtil.findkthToEndNode(head, 6);
 		assertNull(kthNode);
 	}
+	
+	@Test
+	public void testAddNumbers(){
+		LinkedListNode head1=new LinkedListNode("9");
+		head1.addToTail("7");
+		head1.addToTail("8");
+		
+		LinkedListNode head2=new LinkedListNode("6");
+		head2.addToTail("5");
+		head2.addToTail("3");
+		
+		LinkedListNode head=new LinkedListNode("5");
+		head.addToTail("3");
+		head.addToTail("2");
+		head.addToTail("1");
+		
+		LinkedListNode node=head;
+		LinkedListNode result = LinkedListUtil.addNumbers(head1, head2);
+		while(result.getNext()!=null){
+			assertEquals(node.getNext().getData(), result.getNext().getData());
+			node=node.getNext();
+			result=result.getNext();
+			
+		}
+	}
 
 }
