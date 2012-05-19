15eed5a7ab40b18a9bedc4092a292b364580a279
Fei Gao <eaglelion8038@hotmail.com>
2012-05-19 00:36:12 -0700

    add practice 103 and 104 for book AFI

--- main/java/com/ssparrow/afi/ch01searching/Ch01SearchUtil.java
+++ main/java/com/ssparrow/afi/ch01searching/Ch01SearchUtil.java
@@ -29,7 +29,7 @@ public class Ch01SearchUtil {
 	 * @param value
 	 * @return
 	 */
-	public static int p101FIndSquareRootFloor(int value){
+	public static int p101FindSquareRootFloor(int value){
 		int start=0;
 		int end=65536;
 		
@@ -50,6 +50,11 @@ public class Ch01SearchUtil {
 		return start;
 	}
 	
+	/**
+	 * @param array
+	 * @param value
+	 * @return
+	 */
 	public static int p102BinarySearch(int[] array, int value){
 		int start=0;
 		int end=array.length-1;
@@ -66,5 +71,71 @@ public class Ch01SearchUtil {
 		}
 		
 		return -1;
+	}
+	
+	/**
+	 * @return
+	 */
+	public static int p103FindNumberCeling(int[] array, int value){
+		
+		if(array[0]>value){
+			return 0;
+		}else if(array[array.length-1]<=value){
+			return -1;
+		}
+		
+		int start=0;
+		int end=array.length-1;
+		
+		return p103FindNumberCeling(array, value, start, end);
+	}
+	
+	private static int p103FindNumberCeling(int[] array, int value, int start, int end){
+		if(start>end){
+			return -1;
+		}else if(end==start+1){
+			if(array[start]>value && array[end]>=value){
+				return start;
+			}else if(array[start]<value &&array[end]>value){
+				return end;
+			}else if(array[start]<value &&array[end]<value){
+				return -1;
+			}
+		}
+		
+		int mid=start+(end-start)/2;
+		
+		if(array[mid]==value){
+			return mid+1;
+		}else if(array[mid]<value){
+			return p103FindNumberCeling(array, value, mid, end);
+		}else{
+			return p103FindNumberCeling(array, value, start, mid);
+		}
+	}
+	
+	/**
+	 * @param array
+	 * @param start
+	 * @param end
+	 * @return
+	 */
+	public static int p104FindMagicIndexWithDuplicate(int [] array, int start, int end){
+		if(start>end){
+			return -1;
+		}
+		
+		int mid=start+(end-start)/2;
+		if(array[mid]==mid){
+			return mid;
+		}
+		
+		int left=Math.min(array[mid], mid-1);
+		int leftMatch = p104FindMagicIndexWithDuplicate(array, start, left);
+		if(leftMatch>=0){
+			return leftMatch;
+		}
+		int right=Math.max(array[mid], mid+1);
+		return p104FindMagicIndexWithDuplicate(array, right, end);
 	}
 }
--- test/java/com/ssparrow/afi/ch01searching/Ch01SearchUtilTest.java
+++ test/java/com/ssparrow/afi/ch01searching/Ch01SearchUtilTest.java
@@ -29,25 +29,25 @@ public class Ch01SearchUtilTest {
 
 	@Test
 	public void testP101FindSquareRootFloor() {
-		int squreRoot=Ch01SearchUtil.p101FIndSquareRootFloor(0);
+		int squreRoot=Ch01SearchUtil.p101FindSquareRootFloor(0);
 		assertEquals(0, squreRoot);
 
-		squreRoot=Ch01SearchUtil.p101FIndSquareRootFloor(1);
+		squreRoot=Ch01SearchUtil.p101FindSquareRootFloor(1);
 		assertEquals(1, squreRoot);
 		
-		squreRoot=Ch01SearchUtil.p101FIndSquareRootFloor(3);
+		squreRoot=Ch01SearchUtil.p101FindSquareRootFloor(3);
 		assertEquals(1, squreRoot);
 		
-		squreRoot=Ch01SearchUtil.p101FIndSquareRootFloor(4);
+		squreRoot=Ch01SearchUtil.p101FindSquareRootFloor(4);
 		assertEquals(2, squreRoot);
 		
-		squreRoot=Ch01SearchUtil.p101FIndSquareRootFloor(5);
+		squreRoot=Ch01SearchUtil.p101FindSquareRootFloor(5);
 		assertEquals(2, squreRoot);
 		
-		squreRoot=Ch01SearchUtil.p101FIndSquareRootFloor(121);
+		squreRoot=Ch01SearchUtil.p101FindSquareRootFloor(121);
 		assertEquals(11, squreRoot);
 		
-		squreRoot=Ch01SearchUtil.p101FIndSquareRootFloor(125);
+		squreRoot=Ch01SearchUtil.p101FindSquareRootFloor(125);
 		assertEquals(11, squreRoot);
 		
 	}
@@ -65,6 +65,45 @@ public class Ch01SearchUtilTest {
 		assertEquals(5, Ch01SearchUtil.p102BinarySearch(array, 12));
 
 		assertEquals(9, Ch01SearchUtil.p102BinarySearch(array, 100));
+	}
+	
+	@Test
+	public void testP103FindNumberCeling(){
+		int [] array=new int[]{2,3,5,7,10,12,16,30,50,100};
+		
+		assertEquals(0, Ch01SearchUtil.p103FindNumberCeling(array, 0));
+
+		assertEquals(0, Ch01SearchUtil.p103FindNumberCeling(array, 1));
+
+		assertEquals(3, Ch01SearchUtil.p103FindNumberCeling(array, 5));
+
+		assertEquals(4, Ch01SearchUtil.p103FindNumberCeling(array, 8));
+		
+		assertEquals(9, Ch01SearchUtil.p103FindNumberCeling(array, 60));
+		
+		assertEquals(-1, Ch01SearchUtil.p103FindNumberCeling(array, 100));
+		
+		assertEquals(-1, Ch01SearchUtil.p103FindNumberCeling(array, 100));
+	}
+	
+	@Test
+	public void testP104FindMagicIndexWithDuplicate(){
+		int [] array=new int []{-3,-1,0,1,3,5,7,9,11,20,30,40,50};
+		
+		assertEquals(5, Ch01SearchUtil.p104FindMagicIndexWithDuplicate(array, 0, array.length-1));
+		
+		array=new int []{-3,-1,0,1,3,6,7,9,11,20,30,40,50};
+		
+		assertEquals(-1, Ch01SearchUtil.p104FindMagicIndexWithDuplicate(array, 0, array.length-1));
+		
+		array=new int []{0,-1,0,1,3,6,7,9,11,20,30,40,50};
+		
+		assertEquals(0, Ch01SearchUtil.p104FindMagicIndexWithDuplicate(array, 0, array.length-1));
+		
+
+		array=new int []{-10,1,1,1,3,3,5,7,9,11,20,20,20,20};
+		
+		assertEquals(1, Ch01SearchUtil.p104FindMagicIndexWithDuplicate(array, 0, array.length-1));
 	}
 
 }
