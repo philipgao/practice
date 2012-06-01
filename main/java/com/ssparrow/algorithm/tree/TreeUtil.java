8ab73c18dca2aee288143e2cc2b7ff4cf6462bae
Fei Gao <eaglelion8038@hotmail.com>
2012-05-31 21:47:36 -0400

    add comment

--- main/java/com/ssparrow/algorithm/tree/TreeUtil.java
+++ main/java/com/ssparrow/algorithm/tree/TreeUtil.java
@@ -195,6 +195,14 @@ public class TreeUtil {
 	}
 	
 	/**
+	 * Consider this string representation for binary trees. Each node is of the form (lr), where l represents the left child and r represents the right child. 
+	 * If l is the character 0, then there is no left child. Similarly, if r is the character 0, then there is no right child. 
+	 * Otherwise, the child can be a node of the form (lr), and the representation continues recursively.
+	 * For example: (00) is a tree that consists of one node. ((00)0) is a two-node tree in which the root has a left child, and the left child is a leaf. 
+	 * And ((00)(00)) is a three-node tree, with a root, a left and a right child.
+	 * 
+	 * Write a function that takes as input such a string, and returns -1 if the string is malformed, and the depth of the tree if the string is well-formed.
+	 * 
 	 * @param str
 	 * @return
 	 */
